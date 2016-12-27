bf3c7e089117b824a2f4219f6547740ffc2c104b
“nataliu” <13636424061@163.com>
2016-12-27 21:11:03 +0800

    implement group relation

--- db/migrate/20161227130409_create_group_relationships.rb
+++ db/migrate/20161227130409_create_group_relationships.rb
@@ -2,8 +2,7 @@ class CreateGroupRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :group_relationships do |t|
       t.integer :group_id
-      t.string :user
-      t.integer :id
+      t.integer :user_id
 
       t.timestamps
     end
